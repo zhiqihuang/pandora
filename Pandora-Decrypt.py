6453db5383e3bd3b10093afe5c1bc4e1bb345e1c
Zhiqi Huang <Charles.hzq@gmail.com>
2016-05-12 01:54:50 -0400

    v1.1

---
+++ Pandora-Decrypt.py
@@ -0,0 +1,55 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from Crypto.Cipher import AES
+import hashlib
+import getpass
+import random
+import base64
+from Crypto.PublicKey import RSA
+
+
+# In[2]:
+
+private_key_location = raw_input("private key localtion: ")
+aes_key = getpass.getpass('AES Key: ').encode('utf-8')
+f = open(private_key_location,'r')
+private = f.read()
+f.close()
+f = open('pandora.txt','r')
+pw_list = f.read().splitlines() 
+f.close()
+
+
+# In[12]:
+
+def decode(private,aes_key,pw_list):
+    private_key = RSA.importKey(private)
+    md = hashlib.md5()
+    md.update(aes_key)
+    aes_string = md.digest()
+    obj = AES.new(aes_string)
+    pandora = []
+    for string in pw_list:
+        account = private_key.decrypt(obj.decrypt(base64.b64decode(string.split('\t')[0])))
+        login = private_key.decrypt(obj.decrypt(base64.b64decode(string.split('\t')[1])))
+        pandora.append([account,login])
+    return pandora
+
+
+# In[16]:
+
+pandora = decode(private,aes_key,pw_list)
+print "!!Warning!! Pandora is opened"
+opt = raw_input("save or print: ")
+if opt == 'save':
+    print "saved to the same dirctory with private key. !!Delete after viewing!!"
+    f=open('/'.join(private_key_location.split('/')[0:(len(private_key_location.split('/'))-1)])+'/pandora-opened.txt','w')
+    for i in pandora:
+        f.write("""%s,%s\n"""%(i[0],i[1]))
+    f.close()
+else:
+    print pandora
+
--- Pandora-Encrypt.py
+++ Pandora-Encrypt.py
@@ -15,12 +15,15 @@ from Crypto.PublicKey import RSA
 
 f = open('RSA-PublicKey.txt','r')
 public = f.read()
+f.close()
+f = open('pandora.txt','r')
+pw_list = [x.split('\t') for x in f.read().splitlines()]
+f.close()
 account = raw_input('account info: ').encode('utf-8')
 username = raw_input('username: ').encode('utf-8')
 password = getpass.getpass('password: ').encode('utf-8')
 aes_key = getpass.getpass('AES Key: ').encode('utf-8')
-init_string = '|'.join([account,username,password]).encode('utf-8')
-
+init_string = [account.encode('utf-8'),'|'.join([username,password]).encode('utf-8')]
 
 # In[7]:
 
@@ -29,17 +32,27 @@ def encode(public,aes_key,init_string):
     md = hashlib.md5()
     md.update(aes_key)
     aes_string = md.digest()
-    rsa_encoded = public_key.encrypt(init_string,random.randint(0,1990))[0]
+    account_rsa = public_key.encrypt(init_string[0],random.randint(0,1990))[0]
+    login_rsa = public_key.encrypt(init_string[1],random.randint(0,1990))[0]
     obj = AES.new(aes_string)
-    return obj.encrypt(rsa_encoded)
+    return [obj.encrypt(account_rsa),obj.encrypt(login_rsa)]
 
 
 # In[8]:
 
-encoded = base64.b64encode(encode(public,aes_key,init_string))
-f = open('pandora.txt','a')
-f.write("""%s"""%encoded)
-f.write("\n")
+encoded = [base64.b64encode(encode(public,aes_key,init_string)[0]),base64.b64encode(encode(public,aes_key,init_string)[1])]
+if encoded[0] in [x[0] for x in pw_list]:
+	updated_list = [encoded if x[0] == encoded[0] else x for x in pw_list]
+else:
+	pw_list.append(encoded)
+	updated_list = pw_list
+f = open('pandora.txt','w')
+
+for i in updated_list:
+	f.write("""%s"""%i[0])
+	f.write("\t")
+	f.write("""%s"""%i[1])
+	f.write("\n")
 f.close()
 print 'Encrypt into Pandora!'
 
